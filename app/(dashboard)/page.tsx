19da0dd34c8fe98d99d93bfcd1bbfbd04d422fb5
Rajdeep De <surjode2003@gmail.com>
2023-12-11 21:07:44 +0530

    updated the pages and cards

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,106 @@
-import Image from 'next/image'
+import { GetFormStats } from "@/actions/form";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { ReactNode, Suspense } from "react";
+import { LuView } from "react-icons/lu";
+import { FaWpforms } from "react-icons/fa";
+import { HiCursorClick } from "react-icons/hi";
+import { TbArrowBounce } from "react-icons/tb";
 
 export default function Home() {
   return (
-    <div>Hello</div>
-  )
+    <div className="container pt-4">
+      <Suspense fallback={<StatsCards loading={true} />}>
+        <CardStatsWrapper />
+      </Suspense>
+    </div>
+  );
+}
+
+async function CardStatsWrapper() {
+  const stats = await GetFormStats();
+  return <StatsCards loading={false} data={stats} />;
+}
+
+interface StatsCardProps {
+  data?: Awaited<ReturnType<typeof GetFormStats>>;
+  loading: boolean;
+}
+function StatsCards(props: StatsCardProps) {
+  const { data, loading } = props;
+
+  return (
+    <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      <StatsCard
+        title="Total visits"
+        icon={<LuView className="text-black dark:text-white" />}
+        helperText="All time form visits"
+        value={data?.visits.toLocaleString() || ""}
+        loading={loading}
+        className="shadow-md shadow-gray-300"
+      />
+      <StatsCard
+        title="Total submissions"
+        icon={<FaWpforms className="text-black dark:text-white" />}
+        helperText="All time form submissions"
+        value={data?.submissions.toLocaleString() || ""}
+        loading={loading}
+        className="shadow-md shadow-gray-300"
+      />
+      <StatsCard
+        title="Submission rate"
+        icon={<HiCursorClick className="text-black dark:text-white" />}
+        helperText="Visits that result in form submission"
+        value={data?.submissionRate.toLocaleString() + "%" || ""}
+        loading={loading}
+        className="shadow-md shadow-gray-300"
+      />
+      <StatsCard
+        title="Bounce Rate"
+        icon={<TbArrowBounce className="text-black dark:text-white" />}
+        helperText="Visits that leaves without interacting"
+        value={data?.submissionRate.toLocaleString() + "%" || ""}
+        loading={loading}
+        className="shadow-md shadow-gray-300"
+      />
+    </div>
+  );
+}
+
+function StatsCard({
+  title,
+  value,
+  icon,
+  helperText,
+  loading,
+  className,
+}: {
+  title: string;
+  value: string;
+  helperText: string;
+  className: string;
+  loading: boolean;
+  icon: ReactNode;
+}) {
+  return (
+    <Card className={className}>
+      <CardHeader className="flex flex-row items-center justify-between pb-2">
+        <CardTitle className="text-sm font-medium text-muted-foreground">
+          {title}
+        </CardTitle>
+        {icon}
+      </CardHeader>
+      <CardContent>
+        <div className="text-2-xl font-bold">
+          {loading && (
+            <Skeleton>
+              <span className="opacity-0">0</span>
+            </Skeleton>
+          )}
+          {!loading && value}
+        </div>
+        <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
+      </CardContent>
+    </Card>
+  );
 }
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,7 +5,7 @@ function Logo() {
   return (
     <Link
      href={'/'}
-     className='font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer'
+     className='font-bold text-3xl bg-black dark:bg-white text-transparent bg-clip-text hover:cursor-pointer'
     
     >PageForge</Link>
   )
