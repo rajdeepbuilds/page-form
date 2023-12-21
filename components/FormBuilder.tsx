4462ffbdd13f16ce7bd77c38914fadc797677ae5
Rajdeep De <surjode2003@gmail.com>
2023-12-21 20:14:29 +0530

    added dnd kit and added required svgs

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -5,31 +5,36 @@ import React from 'react'
 import PreviewDialogBtn from './PreviewDialogBtn';
 import SaveFormBtn from './SaveFormBtn';
 import PublishFormBtn from './PublishFormBtn';
+import Designer from './Designer';
 
 function FormBuilder({form}:{
     form: Form
 }) {
   return (
     <main className="flex flex-col w-full">
-        <div className='flex justify-between border-b-2 p-4 gap-3 items-center'>
-            <h2 className='truncate font-medium'>
-                <span className='text-muted-foreground mr-2'>
-                    Form:
-                </span>
-                {form.name}
-            </h2>
-            <div className="flex items-center gap-2">
-                <PreviewDialogBtn/>
-                {!form.published && (
-                    <>
-                       <SaveFormBtn/>
-                       <PublishFormBtn/>
-                    </>
-                )}
-            </div>
+      <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
+        <h2 className="truncate font-medium">
+          <span className="text-muted-foreground mr-2">Form:</span>
+          {form.name}
+        </h2>
+        <div className="flex items-center gap-2">
+          <PreviewDialogBtn />
+          {!form.published && (
+            <>
+              <SaveFormBtn />
+              <PublishFormBtn />
+            </>
+          )}
         </div>
+      </nav>
+      <div
+        className="flex w-full items-center flex-grow justify-center
+         relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]"
+      >
+        <Designer/>
+      </div>
     </main>
-  )
+  );
 }
 
 export default FormBuilder
