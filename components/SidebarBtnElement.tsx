17ee73b2b941542c1fedcc78c270efbf1123954c
Rajdeep De <surjode2003@gmail.com>
2024-01-01 21:47:58 +0530

    sidebarbtn updated

--- components/SidebarBtnElement.tsx
+++ components/SidebarBtnElement.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { FormElement } from "./FormElements";
 import { Button } from "./ui/button";
+import { useDraggable } from "@dnd-kit/core";
 
 function SidebarBtnElement({ formElement }: { formElement: FormElement }) {
 
     const{label,icon: Icon}=formElement.designerBtnElement;
 
-  return <Button>
+    const draggable = useDraggable({
+        id: `designer-btn-${formElement.type}`,
+        data:{
+            type: formElement.type,
+            isDesignerbtnElement: true,
+        },
+    });
+
+  return <Button variant={"outline"} className="flex flex-col gap-2 h-[120px] w-[120px] cursor-grab">
     <Icon className='h-8 w-8 text-primary cursor-grab'/>
-    <p className="text-xs"></p>
+    <p className="text-xs">{label}</p>
   </Button>;
 }
 
