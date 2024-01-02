18c58578ccd8df73461f37113acaa2376080e72a
Rajdeep De <surjode2003@gmail.com>
2024-01-02 14:12:46 +0530

    dragoverlay needs to rebuild

--- components/DragOverlayWrapper.tsx
+++ components/DragOverlayWrapper.tsx
@@ -1,17 +1,33 @@
-import { DragOverlay, useDndMonitor } from '@dnd-kit/core'
-import React from 'react'
+import { Active, DragOverlay, useDndMonitor } from '@dnd-kit/core'
+import React, { useState } from 'react'
+import SidebarBtnElement, { SidebarBtnElementDragOverlay } from './SidebarBtnElement';
+import { FormElements } from './FormElements';
 
 function DragOverlayWrapper() {
+  const [draggedItem, setdraggedItem] = useState<Active | null>(null);
 
 
     useDndMonitor({
         onDragStart: (event) =>{
-            console.log("DRAG ITEM",event);
-            
+         setdraggedItem(event.active);   
+        },
+        onDragCancel: () => {
+          setdraggedItem(null);
+        },
+        onDragEnd: () => {
+          setdraggedItem(null);
         },
     });
 
-    const node= <div>No Drag Overlay</div>;
+    if (!draggedItem) return null;
+
+    let node= <div>No Drag Overlay</div>;
+    const isSidebarBtnElement = draggedItem.data?.current?.isDesignerBtnElement;
+
+    if (isSidebarBtnElement) {
+      const type= draggedItem.data?.current?.type;
+      node = <SidebarBtnElementDragOverlay formElement={FormElements[type]} />;
+    }
 
   return <DragOverlay>
     {node}
